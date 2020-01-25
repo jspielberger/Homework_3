05db40b74a184f9531909385461e22da3f123bca
Joel Spielberger <joelspielberger101@gmail.com>
2020-01-24 18:23:03 -0800

    Added prompts and arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+//*arrays needed
+lower_alph = []
+upper_alph = []
+special_characters = ['?', '!', '@', '#', '$','%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '`', '|',   '_', '+', ' ','/','>','<', ':', ';' ]
+digits = ['0','1','2','3','5','6','7','8','9',]
+password = ('')
 // Write password to the #password input
 function writePassword() {
+let characters=null;
+  while (characters < 8 || characters > 128 || NaN(characters)) {
+    characters = prompt("Choose a number between 8 and 128");
+  }
+
+  function generatePassword() {
+
+
+    for (let i = 0; i < characters; i++) {
+      var num = Math.floor(Math.random()*characters);
+      
+      password += 'c'
+
+    
+
+  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -10,5 +31,7 @@ function writePassword() {
 
 }
 
+
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
