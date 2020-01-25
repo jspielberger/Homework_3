a1a1e7dea492fa32fc567e1e10d2623c57c27010
Joel Spielberger <joelspielberger101@gmail.com>
2020-01-24 21:20:17 -0800

    Inserted a random character generator, trying to incorporate it into the overall function

--- Develop/script.js
+++ Develop/script.js
@@ -3,46 +3,59 @@ var generateBtn = document.querySelector("#generate");
 //*arrays needed
 
 
-  lower_alph = "abcdefghijklmnopqrstuvwxyz".split('')
-  upper_alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
-  special_characters = ['?', '!', '@', '#', '$','%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '`', '|',   '_', '+', ' ','/','>','<', ':', ';' ]
-  digits = ['0','1','2','3','5','6','7','8','9',]
+lower_alph = "abcdefghijklmnopqrstuvwxyz".split('')
+upper_alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+special_characters = ['?', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '`', '|', '_', '+', ' ', '/', '>', '<', ':', ';']
+digits = ['0', '1', '2', '3', '5', '6', '7', '8', '9',]
+chosen_characters = []
+password = []
 
-  password = ('')
 
+
+function generatecharacter(array) {
+  var num = Math.floor(Math.random() * array.length);
+  randomcharacter = array[num];
+  return randomcharacter
 }
 
-
 // Write password to the #password input
-function writePassword() {
-let characters=null;
+function passwordoptions() {
+  let characters = null;
   while (characters < 8 || characters > 128 || NaN(characters)) {
     characters = parseInt(prompt("Choose a number between 8 and 128"));
   }
-    let lowercase = confirm("Do you want lowercase letters?")
-      if (lowercase) {
-        full_array = full_array.append((lower_alph))
-      }
-      
-    let uppercase = confirm('Do you want upppercase letters?')
-    if (lowercase === "True") {
-      full_array.append((upper_alph))
+
+  lowercase = confirm("Do you want lowercase letters?")
+  if (lowercase) {
+    chosen_characters = chosen_characters.append((lower_alph));
+    password = password.append(generatecharacter(lower_alph));
   }
-    let numbers = confirm('Do you want numbers?')
-    if (numbers) === "True"
-    let special = confirm('Do you want special characters?(e.g. ?, !, @)')
-  function generatePassword() {
 
-
-    for (let i = 0; i < characters; i++) {
-      var num = Math.floor(Math.random()*characters);
-    return num
-      password += 'c'
-
-    
-
+  upper = confirm('Do you want uppercase letters?')
+  if (upper) {
+    chosen_characters = chosen_characters.append((upper_alph));
+    password = password.append(generatecharacter(upper_alph));
   }
-  var password = generatePassword();
+  numbers = confirm('Do you want numbers?')
+  if (numbers) {
+  chosen_characters = chosen_characters.append((digits));
+  password = password.append(generatecharacter(digits));
+  }
+  special = confirm('Do you want special characters?(e.g. ?, !, @)')
+  if (special) {
+  chosen_characters = chosen_characters.append((special_characters));
+  password = password.append(generatecharacter(special_characters));
+  }
+  function writePassword() {
+    for (let i = 0; i < characters.length; i++) {
+      let password_character = generatecharacter(chosen_characters);
+      password.push(password_character);
+    }
+
+    return password.join('');
+  }
+}
+rd = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
