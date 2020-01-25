0a514001492b26a4a8490a2f11f9b52ae9a1f6fb
Joel Spielberger <joelspielberger101@gmail.com>
2020-01-24 19:40:16 -0800

    Took out most prompts, put in confirms

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //*arrays needed
-lower_alph = []
-upper_alph = []
-special_characters = ['?', '!', '@', '#', '$','%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '`', '|',   '_', '+', ' ','/','>','<', ':', ';' ]
-digits = ['0','1','2','3','5','6','7','8','9',]
-password = ('')
+
+
+  lower_alph = "abcdefghijklmnopqrstuvwxyz".split('')
+  upper_alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+  special_characters = ['?', '!', '@', '#', '$','%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '`', '|',   '_', '+', ' ','/','>','<', ':', ';' ]
+  digits = ['0','1','2','3','5','6','7','8','9',]
+
+  password = ('')
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
 let characters=null;
   while (characters < 8 || characters > 128 || NaN(characters)) {
-    characters = prompt("Choose a number between 8 and 128");
+    characters = parseInt(prompt("Choose a number between 8 and 128"));
   }
-
+    let lowercase = confirm("Do you want lowercase letters?")
+      if (lowercase) {
+        full_array = full_array.append((lower_alph))
+      }
+      
+    let uppercase = confirm('Do you want upppercase letters?')
+    if (lowercase === "True") {
+      full_array.append((upper_alph))
+  }
+    let numbers = confirm('Do you want numbers?')
+    if (numbers) === "True"
+    let special = confirm('Do you want special characters?(e.g. ?, !, @)')
   function generatePassword() {
 
 
     for (let i = 0; i < characters; i++) {
       var num = Math.floor(Math.random()*characters);
-      
+    return num
       password += 'c'
 
     
