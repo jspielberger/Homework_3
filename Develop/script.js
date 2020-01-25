7813e67f2f60e0ab54fb2104d936090ea16fa7da
Joel Spielberger <joelspielberger101@gmail.com>
2020-01-24 21:47:22 -0800

    sorted the functions with each other into a larger function that is enacted when a user hits the button,

--- Develop/script.js
+++ Develop/script.js
@@ -8,61 +8,56 @@ upper_alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
 special_characters = ['?', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '`', '|', '_', '+', ' ', '/', '>', '<', ':', ';']
 digits = ['0', '1', '2', '3', '5', '6', '7', '8', '9',]
 chosen_characters = []
-password = []
+password = ['']
 
 
 
 function generatecharacter(array) {
   var num = Math.floor(Math.random() * array.length);
   randomcharacter = array[num];
-  return randomcharacter
+  return randomcharacter;
 }
 
 // Write password to the #password input
-function passwordoptions() {
+function generatepassword() {
   let characters = null;
   while (characters < 8 || characters > 128 || NaN(characters)) {
     characters = parseInt(prompt("Choose a number between 8 and 128"));
   }
 
-  lowercase = confirm("Do you want lowercase letters?")
+  let lowercase = confirm("Do you want lowercase letters?");
   if (lowercase) {
     chosen_characters = chosen_characters.append((lower_alph));
     password = password.push(generatecharacter(lower_alph));
   }
 
-  upper = confirm('Do you want uppercase letters?')
+  let upper = confirm('Do you want uppercase letters?');
   if (upper) {
     chosen_characters = chosen_characters.append((upper_alph));
     password = password.push(generatecharacter(upper_alph));
   }
-  numbers = confirm('Do you want numbers?')
+  let numbers = confirm('Do you want numbers?');
   if (numbers) {
   chosen_characters = chosen_characters.append((digits));
   password = password.push(generatecharacter(digits));
   }
-  special = confirm('Do you want special characters?(e.g. ?, !, @)')
+  let special = confirm('Do you want special characters?(e.g. ?, !, @)');
   if (special) {
   chosen_characters = chosen_characters.append((special_characters));
   password = password.push(generatecharacter(special_characters));
   }
   
-  for (let i = 0; i < characters.length; i++) {
+  for (let i = 0; i < characters; i++) {
       let password_character = generatecharacter(chosen_characters);
       password.push(password_character);
   }
-
-    return password.join('');
-  }
-}
-rd = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  return password.join('');
 }
 
-
+function writePassword() {
+  returned_password = generatepassword()
+  let returned_password_text = document.querySelector("#password")
+  returned_password_text.value = returned_password;
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
