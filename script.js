884e413431a436775b4c137147b9780517113277
Joel Spielberger <joelspielberger101@gmail.com>
2020-01-25 01:50:11 -0800

    fixed the inherent character bug by reordering the default password loop at the end of the functions script...everything should be in tip top shape

--- script.js
+++ script.js
@@ -2,29 +2,30 @@
 let generateBtn = document.querySelector("#generate");
 //*arrays needed
 
-let lower_alph = 'abcdefghijklmnopqrstuvwxyz'.split('');
-let upper_alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-let special_characters = ['?', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '`', '|', '_', '+', ' ', '/', '>', '<', ':', ';'];
-let digits = ['0', '1', '2', '3', '5', '6', '7', '8', '9'];
-let password = [''];
+
+
 
 
 
   // Write password to the #password input
 let generatepassword = function(){
+  let lower_alph = 'abcdefghijklmnopqrstuvwxyz'.split('');
+let upper_alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+let special_characters = ['?', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '`', '|', '_', '+', ' ', '/', '>', '<', ':', ';'];
+let digits = ['0', '1', '2', '3', '5', '6', '7', '8', '9'];
   let chosen_characters = [];
-  let default_password = [];
+  let default_password = [''];
+  let password = [''];
 
   function generatecharacter(array) {
-    let num = Math.floor(Math.random()* (array.length - 1));
+    let num = Math.floor(Math.random()* array.length);
     let randomcharacter = array[num];
 
     return randomcharacter;
   }
-    let character_length = parseInt(prompt("Time to decide how big your password will be. Please choose a number between 8 and 128")
-    );
+    let character_length = parseInt(prompt("Great news! It's time to decide how big your password will be! Remember, you need to choose a number between 8 and 128!"));
     if (isNaN(character_length) === true) {
-      alert('Nuh, uh! Password length must be provided as a number');
+      alert('Nuh, uh! Password length must be provided as a number, not a letter silly!');
       return;
     }
   
@@ -84,13 +85,13 @@ let generatepassword = function(){
       alert('Must select at least one character type');
       return;
     }
-    for (var i = 0; i < default_password.length; i++) {
-      password[i] = default_password[i];
-    }
-
+    
     for (let i = 0; i < character_length; i++) {
       let password_character = generatecharacter(chosen_characters);
       password.push(password_character);
+    }
+    for (var i = 0; i < default_password.length; i++) {
+      password[i] = default_password[i];
     }
   return password.join('');
 }
